a492a785902fa19fab2813fa22492a3ddf747f27
endo-kotaro <umaibou1126@gmail.com>
2020-10-04 17:32:13 +0900

    1004

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  # config.hosts << "rails.com"
   config.hosts << "portfoliough.com"
 
   # Do not eager load code on boot.
@@ -50,7 +51,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
--- docker/prod/app/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ ! -d "$USER_SSH_KEYS_FOLDER" ] && mkdir -p $USER_SSH_KEYS_FOLDER
-
-echo $PUB_KEY >~/.ssh/authorized_keys
-
-unset PUB_KEY
-
-/usr/sbin/sshd -D
